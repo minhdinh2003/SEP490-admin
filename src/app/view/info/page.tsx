b104b511f4ded7db88730f9b4975ac65903bd3e7
Linhlon1904 <linhdead1904@gmail.com>
2025-02-21 10:43:14 +0700

    fix lib

--- src/app/view/info/page.tsx
+++
@@ -1,9 +0,0 @@
-import OverViewPage from '@/features/overview/components/overview';
-
-export const metadata = {
-  title: 'View : info'
-};
-
-export default function page() {
-  return <OverViewPage />;
-}
