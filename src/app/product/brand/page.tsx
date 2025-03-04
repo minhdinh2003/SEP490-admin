cb354c86a625ab877d9d35b553e247a5b5bc5fda
DuyDQHE163434 <133103039+DuyDQHE163434@users.noreply.github.com>
2025-03-05 00:31:34 +0700

    update product

---
+++ src/app/product/brand/page.tsx
@@ -0,0 +1,38 @@
+'use client';
+import PageContainer from '@/components/layout/page-container';
+import { buttonVariants } from '@/components/ui/button';
+import { Heading } from '@/components/ui/heading';
+import { Separator } from '@/components/ui/separator';
+import { searchParamsCache, serialize } from '@/lib/searchparams';
+import { cn } from '@/lib/utils';
+import { Plus } from 'lucide-react';
+import Link from 'next/link';
+import { SearchParams } from 'nuqs/server';
+import { useAuthStore } from '@/store/use-auth-store';
+import BrandListingPage from '@/features/brand/components/brand-listing';
+
+type pageProps = {
+  searchParams: SearchParams;
+};
+
+export default function Page({ searchParams }: pageProps) {
+  searchParamsCache.parse(searchParams);
+  const { fullName } = useAuthStore.getState();
+  return (
+    <PageContainer>
+      <div className='space-y-4'>
+        <div className='flex items-start justify-between'>
+          <Heading title='Hãng xe' description='Các hãng xe lưu hàng' />
+          <Link
+            href='/product/brand/new'
+            className={cn(buttonVariants(), 'text-xs md:text-sm')}
+          >
+            <Plus className='mr-2 h-4 w-4' /> Thêm
+          </Link>
+        </div>
+        <Separator />
+        <BrandListingPage />
+      </div>
+    </PageContainer>
+  );
+}
