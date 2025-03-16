3f9a7a6740aa8007a7703f4e11d9a027dbfa71d2
DuyDQHE163434 <133103039+DuyDQHE163434@users.noreply.github.com>
2025-03-17 00:06:14 +0700

    fix debounce

--- src/hooks/use-debounce.tsx
+++ src/hooks/use-debounce.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-
+//x
 export function useDebounce<T>(value: T, delay: number): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
