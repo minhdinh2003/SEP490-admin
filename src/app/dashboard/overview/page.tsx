846d564363aecd6f326fa028a73bc27bc9234048
DuyDQHE163434 <133103039+DuyDQHE163434@users.noreply.github.com>
2025-03-25 00:33:11 +0700

    update dashboard

--- src/app/dashboard/overview/page.tsx
+++ src/app/dashboard/overview/page.tsx
@@ -3,7 +3,7 @@ import OverViewPage from '@/features/overview/components/overview';
 export const metadata = {
   title: 'Dashboard : Overview'
 };
-
+//x
 export default function page() {
   return <OverViewPage />;
 }
