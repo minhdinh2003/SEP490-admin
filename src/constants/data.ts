48f85881c70d7b2cac8ca7b08819dabd476670b6
DuyDQHE163434 <133103039+DuyDQHE163434@users.noreply.github.com>
2025-05-10 15:17:55 +0700

    fix constants

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -1,3 +1,4 @@
+import { UserRole } from '@/enum/user-role';
 import { NavItem } from 'types';
 
 export const navItems: NavItem[] = [
@@ -7,6 +8,7 @@ export const navItems: NavItem[] = [
     icon: 'dashboard',
     isActive: false,
     shortcut: ['d', 'd'],
+    roles: [UserRole.ADMIN],
     items: []
   },
   {
@@ -14,46 +16,75 @@ export const navItems: NavItem[] = [
     url: '#',
     icon: 'product',
     isActive: false,
+    roles: [UserRole.ADMIN],
     items: [
       {
         title: 'Hãng xe',
-        url: '/dashboard/product',
+        url: '/product/brand',
         icon: 'userPen',
+        roles: [UserRole.ADMIN],
         shortcut: ['p', 'c']
       },
       {
         title: 'Xe',
         shortcut: ['p', 'c'],
-        url: '/',
+        url: '/product/car',
+        roles: [UserRole.ADMIN],
         icon: 'login'
       },
       {
         title: 'Phụ tùng xe',
         shortcut: ['p', 'd'],
-        url: '/',
+        url: '/product/part',
+        roles: [UserRole.ADMIN],
         icon: 'login'
       }
     ]
+  },
+  {
+    title: 'Người dùng',
+    url: '/user',
+    icon: 'user',
+    isActive: false,
+    roles: [UserRole.ADMIN]
+  },
+  {
+    title: 'Task',
+    url: '/task-template',
+    icon: 'kanban',
+    isActive: false,
+    roles: [UserRole.ADMIN]
   },
   {
     title: 'Tài khoản',
     url: '#',
     icon: 'billing',
     isActive: true,
-
+    roles: [],
     items: [
       {
         title: 'Thông tin',
         url: '/dashboard/profile',
         icon: 'userPen',
+        roles: [],
         shortcut: ['m', 'm']
       },
       {
         title: 'Đăng nhập',
         shortcut: ['l', 'l'],
         url: '/',
+        roles: [],
         icon: 'login'
       }
     ]
   }
 ];
+
+export const filterNavItems = (items: NavItem[], role: UserRole): NavItem[] => {
+  return items
+    .filter((item) => item.roles.includes(role)) // Lọc các mục chính
+    .map((item) => ({
+      ...item,
+      items: item.items ? filterNavItems(item.items, role) : [] // Lọc các mục con
+    }));
+};
