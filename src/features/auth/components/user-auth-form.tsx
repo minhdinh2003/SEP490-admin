35f52b14ad0a2241bd9372bff1defab5efbcef2b
DuyDQHE163434 <133103039+DuyDQHE163434@users.noreply.github.com>
2025-05-20 15:13:39 +0700

    fix features auth

--- src/features/auth/components/user-auth-form.tsx
+++ src/features/auth/components/user-auth-form.tsx
@@ -10,35 +10,27 @@ import {
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { signIn } from 'next-auth/react';
 import { useSearchParams } from 'next/navigation';
 import { useTransition } from 'react';
 import { useForm } from 'react-hook-form';
 import { toast } from 'sonner';
 import * as z from 'zod';
-
+import { useAuthStore } from '@/store/use-auth-store';
+import { useRouter } from 'next/navigation';
 const formSchema = z.object({
   email: z.string().email({ message: 'Nhập địa chỉ email' }),
-  password: z
-    .string()
-    .min(8, 'Mật khẩu phải có ít nhất 8 ký tự')
-    .regex(/[A-Z]/, 'Mật khẩu phải chứa ít nhất một chữ cái viết hoa')
-    .regex(/[a-z]/, 'Mật khẩu phải chứa ít nhất một chữ cái viết thường')
-    .regex(/[0-9]/, 'Mật khẩu phải chứa ít nhất một chữ số')
-    .regex(
-      /[@$!%*?&]/,
-      'Mật khẩu phải chứa ít nhất một ký tự đặc biệt (@, $, !, %, *, ?, &)'
-    )
+  password: z.string().min(1, 'Mật khẩu phải có ít nhất 8 ký tự')
 });
 
 type UserFormValue = z.infer<typeof formSchema>;
 
 export default function UserAuthForm() {
   const searchParams = useSearchParams();
+  const router = useRouter();
   const callbackUrl = searchParams.get('callbackUrl');
   const [loading, startTransition] = useTransition();
   const defaultValues = {
-    email: 'nguyenvana@gmail.com'
+    email: ''
   };
   const form = useForm<UserFormValue>({
     resolver: zodResolver(formSchema),
@@ -46,13 +38,17 @@ export default function UserAuthForm() {
   });
 
   const onSubmit = async (data: UserFormValue) => {
-    startTransition(() => {
-      signIn('credentials', {
-        email: data.email,
-        callbackUrl: callbackUrl ?? '/dashboard'
-      });
-      toast.success('Signed In Successfully!');
+    const { logIn, fullName, userId } = useAuthStore.getState();
+    var result = await logIn({
+      email: data.email,
+      password: data.password
     });
+    if (result.success) {
+      toast.success('Đăng nhập thành công');
+      router.push('/dashboard/overview');
+    } else {
+      toast.error('Đăng nhập thất bại');
+    }
   };
 
   return (
